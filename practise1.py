1ab92808e96028eedb4dec92f6cb221aaae9607a
ANCHATGERIMEGHA <46773055+ANCHATGERIMEGHA@users.noreply.github.com>
2019-01-17 16:18:46 +0530

    Add files via upload

---
+++ practise1.py
@@ -0,0 +1,111 @@
+# my_list=[]      #creating list
+# my_list =[1,2]
+# my_list=["hello",3.4]
+# my_list=["mouse",['a'],3.56,[8,4,6]] #nested list
+
+
+# my_list=['p','o','w','e','r']
+# print(my_list[0])
+# print(my_list[-1]) #negative indexing
+
+
+# n_list=["happy",[2,0,5]]
+# print(n_list[0][1])
+# print(n_list[1][2])         # acessing elemnts frm indexing
+
+
+# my_list=['m','e','g','h','a','n','c','h','t']
+# print(my_list[:])
+# print(my_list[1:])
+# print(my_list[1:2])                 #slicing
+# print(my_list[2:2])
+# print(my_list[1:3])
+# print(my_list[1:1])
+# print(my_list[3:4])
+
+
+
+# my_list=[1,2,3,4,5]
+# my_list[0]=11
+# print(my_list)                            #change elements
+
+
+
+# my_list=[1,2]
+# my_list.append(7)                             # APPEND add elements
+# print(my_list)
+
+# my_list = [1,7,8]
+# my_list.extend([5])                          #Extend
+# print(my_list)
+
+
+
+# my_list1=[1,2]
+# my_list2=[4,5]
+# print(my_list1 + my_list2)                       #concatenate elements
+
+
+
+
+# my_list =[1,2,4]
+# my_list.insert(1,5)            #inserting multiple elements
+# print(my_list)
+
+
+
+
+# my_list=[1,2,3]
+# odd=[7]
+#
+# print(my_list + odd)
+# print(["re"] *3)                                     # * repeats number of times
+
+
+
+# my_list=[1,2,3,4,5]
+# # my_list[2:3] = [5,7]            #insert and squessing in empty list      [2:2] returns empty list
+# # print(my_list)
+
+
+
+
+# my_list=[1,2,3,4,5]
+# del my_list[1:2]                                    #delete frm list using slice operation
+# print(my_list)
+
+
+# my_list=['1','b','c']
+# my_list.remove('b')                                   #remove elements
+# print(my_list)
+
+
+# my_list=['p','i','k']
+# print(my_list.pop(0))                        #pop elements
+# print(my_list.clear())                             #clear
+
+
+
+# vowels =['v','o','w','e','l']
+# index =vowels.index('v')                       #find position of element in list using index
+# print(index)
+
+
+# list =['a',('a','b'),[3,4]]
+# index = list.index(('a','b'))
+# print(index)                            #find index of tuple and list inside a list o/p 1   2
+# index = list.index([3,4])
+# print(index)
+
+# list=[2,3,1]
+# list.sort()                                      #sorting
+# print(list)
+
+
+# os=['win','ps']
+# os.reverse()
+# print(os)
+
+
+
+
